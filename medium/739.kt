49c9b1f5f3a814b488dae37bb3c2f4c3b59148d8
Velkha <velkapb99@gmail.com>
2024-05-03 12:51:43 +0200

    Pequeña correccion

--- medium/739.kt
+++ medium/739.kt
@@ -1,17 +1,10 @@
 class Solution {
     /* Se me ha complicado mas de lo devido, y he acabado teniendo que tirar de Pairs */
+    /* Version antigua, consumiendo un poco mas de memoria de la devida
     fun dailyTemperatures(temperatures: IntArray): IntArray {
-        var intArr = nextGreaterElement(temperatures)
-        return intArr
-    }
-    fun nextGreaterElement(nums1: IntArray): IntArray {
         val stack = ArrayDeque<Pair<Int, Int>>() 
-        val newArray = IntArray(nums1.size)
-        /* 
-        stack.isNotEmpty() && stack.last() 
-        nextGreater[stack.removeLast()] 
-        stack.addLast(num)*/
-        for ((i,num) in nums1.withIndex()) {
+        val newArray = IntArray(temperatures.size)
+        for ((i,num) in temperatures.withIndex()) {
             while(stack.isNotEmpty() && stack.last().second < num){
                 newArray[stack.last().first] = i - stack.last().first
                 stack.removeLast()
@@ -19,5 +12,19 @@ class Solution {
             stack.addLast(Pair(i, num))
         }
         return newArray
+    }*/
+    // Version mejorada con una pizca de memoria menos pero la diferencia es minima
+    fun dailyTemperatures(temperatures: IntArray): IntArray {
+        val stack = ArrayDeque<Int>()
+        val newArray  = IntArray(temperatures.size)
+        for (index in temperatures.indices) {
+            while (stack.isNotEmpty() && temperatures[stack.last()] < temperatures[index]) {
+                val prevIndex = stack.removeLast()
+                newArray [prevIndex] = index - prevIndex
+            }
+            stack.addLast(index)
+        }
+        return newArray 
     }
+
 }
